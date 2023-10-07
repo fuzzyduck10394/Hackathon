9c4c763cb7eba66efa4a350ef01384819deedc2d
fuzzyduck10394 <emi@archlinux.localdomain>
2023-10-08 00:53:04 +0200

    jeden

--- lib/menuUser.dart
+++ lib/menuUser.dart
@@ -29,42 +29,144 @@ class MenuUser extends StatelessWidget {
 
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: Container(
+
+          child: Column(
+            children: [
+
+              //tekst 1
+              Container(
+                alignment: Alignment.bottomLeft,
+                height: 80.0,
               
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                Container(
-                  padding: EdgeInsets.only(top: 50),
-                  child:Container(
-                    alignment: Alignment.bottomLeft,
-                  //height: 100.0,
-
-                    child: Text(
-                      'Szukaj...',
-                       style: TextStyle(
-                        fontSize: 30.0,
-                        color: Colors.black,
-                        fontStyle: FontStyle.italic,
-                      )
-                    ),
-
-                    )
+                child: Text(
+                  'Odkryj nowe wydarzenia',
+              
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    // fontStyle: FontStyle.italic,
                   ),
+                ),
+              ),
 
-                  TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                    )
+              // tekst 2
+              Container(
+                alignment: Alignment.bottomLeft,
+                //height: 100.0,
+              
+                child: Text(
+                  'i pomagaj innym.',
+              
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.black,
+                    // fontWeight: FontWeight.bold,
+                    // fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 20,),
+
+              Container( // text field
+                height: 60,
+                
+                child: TextField(
+                  decoration: InputDecoration(
+                    // icon:
+                    
+                    
+                    hintText: 'Szukaj...',
+                    border: OutlineInputBorder(),
                   )
-                ]
-    
-              )
+                ),
+              ),
+
+              SizedBox(height: 30,),
+
+              Event('Tytul', 'Opis tutaj fiuwajiof ttaki ifhaijoaw opis jest taki opis jest sjfasfk taki opis jesttaki ifhaijoaw opis jest taki opis jest sjfasfk taki opis jestaki ifhaijoaw opis jest taki opis jest sjfasfk taki opis jest', [category('tak')]),
+              SizedBox(height: 20,),
+              Event('Tytul2', 'Opis2taki ifhaijoaw opis jest taki opis jest sjfasfk taki opis jesttaki ifhaijoaw opis jest taki opis jest sjfasfk taki opis jesttaki ifhaijoaw opis jest taki opis jest sjfasfk taki opis jest', [category('tak'), category('nie')]),
+            ],           
+
           ),
-          
+
         ),
 
       
     );
+  }  
+}
+
+class category extends StatelessWidget {
+  final String arg;
+
+  category(this.arg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: Colors.blue,
+      ),
+
+      padding: EdgeInsets.all(3.0),
+
+      child: Text(
+        arg,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 12,
+        )
+      )
+    );
   }
 }
+
+class Event extends StatelessWidget{
+  final String title;
+  final String date;
+  final List<Widget> categories;
+
+  Event(this.title, this.date, this.categories);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: orange,
+      
+      padding: EdgeInsets.all(15.0),
+      
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            )
+          ),
+          Text(
+            date,
+            textAlign: TextAlign.justify,
+            style: TextStyle(
+              fontSize: 11,
+            )
+          ),
+          Row(
+            children:categories,
+          ),
+        ],
+      ),
+
+      
+    );
+  }
+
+}
+
